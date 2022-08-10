161fe25b6cba4dc3e5c5180c5ca72fe6ebab46d5
Rahul Dhodapkar <rahuldhodapkar@Rahuls-MBP.lan>
2022-08-10 07:21:08 -0400

    update with calc folder and tutorial

--- tutorials/load_data_tutorial.py
+++ tutorials/load_data_tutorial.py
@@ -4,13 +4,22 @@
 # Tutorial for loading data and generating cell sentences.
 #
 
+import numpy as np
 import cell2sentence as cs
-import os
 
 chick_values = cs.transforms.read_gbc_csv(
     './data/Chick_retina_atlas_expression_matrix.csv')
+chick_values['gene_names'] = np.array(['chick_' + x for x in chick_values['gene_names']])
 chick_sentences = cs.transforms.trans_expression_matrix(**chick_values)
 chick_vocab = cs.transforms.create_vocabulary_dict(**chick_values)
+
+
+human_values = cs.transforms.read_gbc_csv(
+    './data/Human_retina_combined_all_expression_matrix.csv')
+human_values['gene_names'] = np.array(['human_' + x for x in human_values['gene_names']] )
+
+human_sentences = cs.transforms.trans_expression_matrix(**human_values)
+human_vocab = cs.transforms.create_vocabulary_dict(**human_values)
 
 
 os.path.getsize('./data/Chick_retina_atlas_expression_matrix.csv')
