e58c97589abc0501e98107ad3c944847c7ffa180
Syed Rizvi <syed.rizvi@yale.edu>
2024-08-11 00:13:31 -0400

    added finetuning function for csmodel

--- src/cell2sentence/csmodel.py
+++ src/cell2sentence/csmodel.py
@@ -8,6 +8,7 @@ Main model wrapper class definition
 
 # Python built-in libraries
 import os
+from datetime import datetime
 
 # Third-party libraries
 from datasets import load_from_disk
@@ -54,6 +55,7 @@ class CSModel():
             save_name: name to save model under (no file extension needed)
         """
         self.model_path_or_path = model_path_or_path  # path to model to load
+        self.save_dir = save_dir
 
         # Create save path
         if not os.path.exists(save_dir):
@@ -61,6 +63,18 @@ class CSModel():
         
         save_path = os.path.join(save_dir, save_name)
         self.save_path = save_path
+
+        # Load model, save to disk
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_path_or_path
+        )
+
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path_or_path,
+            cache_dir=os.path.join(save_dir, ".cache"),
+            trust_remote_code=True
+        )
+        model.save_checkpoint(self.save_path)  # TODO: check this
 
     def __str__(self):
         """
@@ -102,6 +116,92 @@ class CSModel():
             batch_size=1000,
         )
 
+        # Define important parameters:
+        block_size = model.config.maximum_context_length  # TODO: check this this is the maximum context length of the LLM (recommended value: 4096)
+
+        def data_collator(examples):
+            # Note: this data collator assumes we are using flash attention, which does not
+            #  support padding. Samples are stacked together to form blocks of input tokens.
+            # concatenate samples since no padding supported
+            all_input_tokens = list(
+                itertools.chain(
+                    *[examples[i]["input_ids"] for i in range(len(examples))]
+                )
+            )
+            all_label_tokens = list(
+                itertools.chain(*[examples[i]["labels"] for i in range(len(examples))])
+            )
+
+            assert len(all_label_tokens) == len(
+                all_input_tokens
+            ), "Mismatch in input ids and labels sizes"
+
+            num_blocks = max(1, min(
+                training_args.max_num_blocks,
+                len(all_label_tokens) // block_size,
+            ))
+            assert num_blocks > 0, "Need more samples to form a block"
+
+            all_input_tokens = torch.tensor(all_input_tokens)[: num_blocks * training_args.block_size]
+            effective_block_size = min(
+                training_args.block_size,
+                all_input_tokens.size(all_input_tokens.dim() - 1),
+            )
+
+            if all_input_tokens.dim() == 1:
+                all_input_tokens = all_input_tokens.unsqueeze(0)
+            all_input_tokens = all_input_tokens.reshape(-1, effective_block_size)
+
+            all_label_tokens = torch.tensor(all_label_tokens)[:num_blocks * training_args.block_size]
+            if all_label_tokens.dim() == 1:
+                all_label_tokens = all_label_tokens.unsqueeze(0)
+            all_label_tokens = all_label_tokens.reshape(-1, effective_block_size)
+            all_attention_mask = torch.ones_like(all_label_tokens)
+
+            return {
+                "input_ids": all_input_tokens,
+                "attention_mask": all_attention_mask,
+                "labels": all_label_tokens,
+            }
+
+        # Define training arguments
+        datetimestamp = datetime.now().strftime('%Y-%m-%d-%H_%M_%S')
+        output_dir = os.path.join(self.save_dir, datetimestamp + f"_finetune_{task}")
+        train_args = TrainingArguments(
+            bf16=True,
+            fp16=False,
+            per_device_train_batch_size=4,
+            per_device_eval_batch_size=4,
+            gradient_accumulation_steps=4,
+            gradient_checkpointing=False,
+            learning_rate=2e-4,
+            load_best_model_at_end=True,
+            logging_steps=50,
+            logging_strategy="steps",
+            lr_scheduler_type="cosine",
+            num_train_epochs=10, 
+            eval_steps=50,
+            evaluation_strategy="steps",
+            save_steps=200,
+            save_strategy="steps",
+            save_total_limit=3,
+            warmup_ratio=0.05,
+            output_dir=output_dir
+        )
+
+        # Define Trainer
+        trainer = Trainer(
+            model=model,
+            args=train_args,
+            data_collator=data_collator,
+            train_dataset=train_dataset,
+            eval_dataset=val_dataset,
+            tokenizer=tokenizer
+        )
+        trainer.train()
+        # TODO: change self.save_path to best model from finetuning
+        print(f"Finetuning completed. Updated model saved to disk at: {self.save_path}")
+
         # TODO/To think about:
         # There might be many ways for people to finetune, e.g. for generation or classification, etc.
         # They will want to finetune on their own dataset of interest.
@@ -129,11 +229,14 @@ class CSModel():
 
         Arguments:
             prompt: a textual prompt.
-            n: the number of toekns to generate given the model supplied.
+            n: the number of tokens to generate given the model supplied.
         Return:
             Text corresponding to the number `n` of tokens requested
         """
         return None
+    
+    def cell_type_prediction_inference(self):
+        pass
 
 
 # Debugging
@@ -161,9 +264,18 @@ if __name__ == "__main__":
     )
 
     # Define CSModel object
+    cell_type_pred_model_path = "/home/sr2464/palmer_scratch/C2S_Files_Syed/multicell_pretraining_v2_important_models/pythia-410m-multicell_v2_2024-07-28_13-55-51_checkpoint-7600_cell_type_pred"
     csmodel = CSModel(
-        model_path_or_path="EleutherAI/pythia-410m",
+        model_path_or_path=cell_type_pred_model_path,
         save_dir="/home/sr2464/palmer_scratch/C2S_Files_Syed/c2s_api_testing/csmodel_testing",
-        save_name="training_run_1_chkpt"
+        save_name="cell_type_pred_pythia_410M_1"
     )
+
+    #--- Start training from a Pythia-410M model ---#
+    # # Define CSModel object
+    # csmodel = CSModel(
+    #     model_path_or_path="EleutherAI/pythia-410m",
+    #     save_dir="/home/sr2464/palmer_scratch/C2S_Files_Syed/c2s_api_testing/csmodel_testing",
+    #     save_name="training_run_1_chkpt"
+    # )
     
